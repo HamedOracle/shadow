67fad8aa7ac3f788b019900c7c256986f4845485
hamed <hddd1382@gmail.com>
2025-05-03 16:47:28 +0330

    0.0.2
    a clean up

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,5 +11,7 @@ func _on_tut_btn_pressed() -> void:
 
 
 
-func _on_lvl_1_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/lvl_1.tscn")
+
+
+func _on_lvl_2_btn_pressed() -> void:
+	pass # Replace with function body.
